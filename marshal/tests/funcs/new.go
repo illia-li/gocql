02d3fd5af567317b7bf03675f4b906df0e408ca6
illia-li <jaforja@gmail.com>
2024-09-21 15:58:20 -0400

    123

---
+++ marshal/tests/funcs/new.go
@@ -0,0 +1,31 @@
+package funcs
+
+import (
+	"bytes"
+	"reflect"
+)
+
+func EqualData(in1, in2 []byte) bool {
+	if in1 == nil || in2 == nil {
+		return in1 == nil && in2 == nil
+	}
+	return bytes.Equal(in1, in2)
+}
+
+func New(in interface{}) interface{} {
+	return getNew(reflect.ValueOf(in))
+}
+
+func getNew(orig reflect.Value) reflect.Value {
+	if orig.Kind() != reflect.Ptr {
+		return reflect.Zero(orig.Type())
+	}
+	if orig.IsNil() {
+		return reflect.Zero(orig.Type())
+	} else if orig.IsZero() {
+		return reflect.Zero(orig.Type())
+	}
+	newVal := reflect.New(orig.Type().Elem())
+	newVal.Elem().Set(getNew(orig.Elem()))
+	return newVal
+}
